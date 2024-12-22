0a60aafb20795e7bac29b67035a2809dba34b7f4
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-22 18:41:23 +0900

    Next.jsとNest.jsの連携(GraphQLの設定)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', //Next.jsからGraphQLサーバーにアクセスできるようにする
+    origin: 'http://localhost:3000', //Next.jsからGraphQLサーバーにアクセスできるようにする
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 許可するHTTPメソッド
+    credentials: true, // クッキーなどの送信を許可
   });
   await app.listen(process.env.PORT ?? 4000);
 }
